772371338baa30ed559cf183e80913eb0c9f4298
viszu <viszu84@gmail.com>
2024-06-12 00:50:40 +0200

    add pokemons view model

---
+++ lib/src/pokemon/view_models/pokemons_view_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pokemons_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$pokemonsViewModelHash() => r'27eabb5d0c4d2db0fa87f825645f611c40787684';
+
+/// See also [PokemonsViewModel].
+@ProviderFor(PokemonsViewModel)
+final pokemonsViewModelProvider = AutoDisposeAsyncNotifierProvider<
+    PokemonsViewModel, List<PokemonApiModel>>.internal(
+  PokemonsViewModel.new,
+  name: r'pokemonsViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$pokemonsViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PokemonsViewModel = AutoDisposeAsyncNotifier<List<PokemonApiModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
