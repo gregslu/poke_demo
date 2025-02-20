6be865514964b6845c871fc187661289c1691c65
gregslu <viszu84@gmail.com>
2025-02-20 20:09:12 +0100

    Temporarily comment out AsyncValueWidget and AsyncValueSliverWidget

--- lib/src/core/common_widgets/async_value_widget.dart
+++ lib/src/core/common_widgets/async_value_widget.dart
@@ -1,69 +1,69 @@
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AsyncValueWidget<T> extends StatelessWidget {
-  const AsyncValueWidget({
-    super.key,
-    required this.value,
-    required this.data,
-    this.skipLoadingOnReload = false,
-    this.skipLoadingOnRefresh = true,
-    this.skipError = false,
-  });
-  final AsyncValue<T> value;
-  final Widget Function(T) data;
-  final bool skipLoadingOnReload;
-  final bool skipLoadingOnRefresh;
-  final bool skipError;
+// class AsyncValueWidget<T> extends StatelessWidget {
+//   const AsyncValueWidget({
+//     super.key,
+//     required this.value,
+//     required this.data,
+//     this.skipLoadingOnReload = false,
+//     this.skipLoadingOnRefresh = true,
+//     this.skipError = false,
+//   });
+//   final AsyncValue<T> value;
+//   final Widget Function(T) data;
+//   final bool skipLoadingOnReload;
+//   final bool skipLoadingOnRefresh;
+//   final bool skipError;
 
-  @override
-  Widget build(BuildContext context) {
-    return value.whenOrNull(
-          skipLoadingOnReload: skipLoadingOnReload,
-          skipLoadingOnRefresh: skipLoadingOnRefresh,
-          skipError: skipError,
-          data: data,
-          // error: (e, st) => Center(
-          //     child: Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: ErrorMessageWidget(error: e),
-          // )),
-          // loading: () => const SizedBox.shrink(),
-        ) ??
-        const SizedBox.shrink();
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return value.whenOrNull(
+//           skipLoadingOnReload: skipLoadingOnReload,
+//           skipLoadingOnRefresh: skipLoadingOnRefresh,
+//           skipError: skipError,
+//           data: data,
+//           // error: (e, st) => Center(
+//           //     child: Padding(
+//           //   padding: const EdgeInsets.all(8.0),
+//           //   child: ErrorMessageWidget(error: e),
+//           // )),
+//           // loading: () => const SizedBox.shrink(),
+//         ) ??
+//         const SizedBox.shrink();
+//   }
+// }
 
-/// Sliver equivalent of [AsyncValueWidget]
-class AsyncValueSliverWidget<T> extends StatelessWidget {
-  const AsyncValueSliverWidget({
-    super.key,
-    required this.value,
-    required this.data,
-    this.skipLoadingOnReload = false,
-    this.skipLoadingOnRefresh = true,
-    this.skipError = false,
-  });
-  final AsyncValue<T> value;
-  final Widget Function(T) data;
-  final bool skipLoadingOnReload;
-  final bool skipLoadingOnRefresh;
-  final bool skipError;
+// /// Sliver equivalent of [AsyncValueWidget]
+// class AsyncValueSliverWidget<T> extends StatelessWidget {
+//   const AsyncValueSliverWidget({
+//     super.key,
+//     required this.value,
+//     required this.data,
+//     this.skipLoadingOnReload = false,
+//     this.skipLoadingOnRefresh = true,
+//     this.skipError = false,
+//   });
+//   final AsyncValue<T> value;
+//   final Widget Function(T) data;
+//   final bool skipLoadingOnReload;
+//   final bool skipLoadingOnRefresh;
+//   final bool skipError;
 
-  @override
-  Widget build(BuildContext context) {
-    return value.whenOrNull(
-          skipLoadingOnReload: skipLoadingOnReload,
-          skipLoadingOnRefresh: skipLoadingOnRefresh,
-          skipError: skipError,
-          data: data,
-          // error: (e, st) => Center(
-          //     child: Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: ErrorMessageWidget(error: e),
-          // )),
-          // loading: () => const SizedBox.shrink(),
-        ) ??
-        const SizedBox.shrink();
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return value.whenOrNull(
+//           skipLoadingOnReload: skipLoadingOnReload,
+//           skipLoadingOnRefresh: skipLoadingOnRefresh,
+//           skipError: skipError,
+//           data: data,
+//           // error: (e, st) => Center(
+//           //     child: Padding(
+//           //   padding: const EdgeInsets.all(8.0),
+//           //   child: ErrorMessageWidget(error: e),
+//           // )),
+//           // loading: () => const SizedBox.shrink(),
+//         ) ??
+//         const SizedBox.shrink();
+//   }
+// }
