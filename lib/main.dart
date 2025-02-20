592df3f608aee67112d3f5774a665f341dc0ebd8
gregslu <viszu84@gmail.com>
2025-02-20 20:07:54 +0100

    Refactor main.dart to use dependency injection initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:poke_demo/src/settings/settings_controller.dart';
+import 'package:poke_demo/src/di.dart';
 
 import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final container = ProviderContainer();
-  await container.read(settingsControllerProvider).loadSettings();
-  runApp(UncontrolledProviderScope(container: container, child: const MyApp()));
+  await di.initialize();
+  runApp(const MyApp());
 }
