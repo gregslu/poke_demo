d1097141b65b18cb27f63a555bb301c741292c36
gregslu <viszu84@gmail.com>
2025-02-20 20:10:14 +0100

    Refactor DemoHelper to use dependency injection

--- lib/src/core/utils/demo_helper.dart
+++ lib/src/core/utils/demo_helper.dart
@@ -1,20 +1,6 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-import '../../pokemon/models/data/pokemon_api_model.dart';
-import '../../pokemon/models/data_sources/pokemon_local_data_source.dart';
-import '../models/data_sources/local_data_source.dart';
-
-part 'demo_helper.g.dart';
-
-@riverpod
-DemoHelper demoHelper(Ref ref) {
-  return DemoHelper(ref);
-}
+import '../../di.dart';
 
 class DemoHelper {
-  DemoHelper(this.ref);
-
   // factory DemoHelper(Ref ref) {
   //   _instance.ref = ref;
   //   return _instance;
@@ -24,8 +10,6 @@ class DemoHelper {
 
   // static final _instance = DemoHelper._();
 
-  final Ref ref;
-
   final bool _artificialDelay = false;
 
   Future<void> artificialDelay([int ms = 2000]) async {
@@ -34,10 +18,7 @@ class DemoHelper {
     }
   }
 
-  Future<void> resetLocal() async {
-    await _local.createAll([]);
+  void resetLocal() {
+    di.local.createAll([]);
   }
-
-  LocalDataSource<PokemonApiModel> get _local =>
-      ref.read(localDataSourceProvider);
 }
