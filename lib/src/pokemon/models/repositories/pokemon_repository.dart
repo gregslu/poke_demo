c7ce5148a9b7136a3349c6d52561e441e8db8e95
gregslu <viszu84@gmail.com>
2025-02-20 20:12:08 +0100

    Refactor PokemonRepository to use dependency injection and simplify implementation

--- lib/src/pokemon/models/repositories/pokemon_repository.dart
+++ lib/src/pokemon/models/repositories/pokemon_repository.dart
@@ -1,73 +1,32 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-import '../../../core/models/data_sources/local_data_source.dart';
-import '../../../core/models/data_sources/remote_data_source.dart';
-import '../../../core/models/repositories/repository.dart';
-import '../../../core/utils/demo_helper.dart';
-import '../../../network/utils/connectivity.dart';
+import '../../../di.dart';
 import '../data/pokemon_api_model.dart';
-import '../data_sources/pokemon_local_data_source.dart';
-import '../data_sources/pokemon_remote_data_source.dart';
 
-part 'pokemon_repository.g.dart';
+class PokemonRepository2 {
+  late final _connectivity = di.connectivity;
+  late final _local = di.local;
+  late final _remote = di.remote;
 
-@riverpod
-FutureOr<PokemonApiModel> pokemon(Ref ref, int pokemonId) async {
-  final pokemonRepository = ref.watch(pokemonRepositoryProvider);
-  return pokemonRepository.read(pokemonId);
-}
-
-class PokemonRepository implements Repository<PokemonApiModel> {
-  PokemonRepository(this.ref);
-
-  final Ref ref;
-
-  @override
-  Future<void> delete(int id) {
-    // TODO: implement delete
-    throw UnimplementedError();
-  }
-
-  @override
   Future<void> deleteLast() async {
-    // if ((await _local.readAll()).isEmpty) {
-    //   return;
-    // }
-    await _demoHelper.artificialDelay();
     await _local.deleteLast();
   }
 
-  @override
-  Future<PokemonApiModel> read(int id) async {
-    // Artificial delay
-    await _demoHelper.artificialDelay();
-    if (await _connectivity.isConnected()) {
-      final pokemon = await _remote.read(id);
-      await _local.create(pokemon);
-      // return pokemon;
-      return await _local.read(id);
+  Future<void> create(int id) async {
+    if (!await _connectivity.isConnected()) {
+      throw Exception('No internet connection');
     }
-    throw Exception('No internet connection');
+    final pokemon = await _remote.read(id);
+    await _local.create(pokemon);
   }
 
-  @override
-  Future<List<PokemonApiModel>> readAll() {
+  PokemonApiModel read(int id) {
+    return _local.read(id);
+  }
+
+  Future<void> update(PokemonApiModel pokemon) async {
+    await _local.update(pokemon);
+  }
+
+  List<PokemonApiModel> readAll() {
     return _local.readAll();
   }
-
-  LocalDataSource<PokemonApiModel> get _local =>
-      ref.read(localDataSourceProvider);
-
-  RemoteDataSource<PokemonApiModel> get _remote =>
-      ref.read(remoteDataSourceProvider);
-
-  DemoHelper get _demoHelper => ref.read(demoHelperProvider);
-
-  Connectivity get _connectivity => ref.read(connectivityProvider);
-}
-
-@riverpod
-Repository<PokemonApiModel> pokemonRepository(Ref ref) {
-  return PokemonRepository(ref);
 }
