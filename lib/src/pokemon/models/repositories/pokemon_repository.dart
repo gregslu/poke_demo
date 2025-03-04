485addaf3ac4b92e78b23abf578057503d05f158
gregslu <viszu84@gmail.com>
2025-03-04 16:33:46 +0100

    Add ValueNotifier to PokemonRepository2 for reactive state management

--- lib/src/pokemon/models/repositories/pokemon_repository.dart
+++ lib/src/pokemon/models/repositories/pokemon_repository.dart
@@ -1,13 +1,35 @@
+import 'package:flutter/foundation.dart';
+
 import '../../../di.dart';
 import '../data/pokemon_api_model.dart';
 
 class PokemonRepository2 {
   late final _connectivity = di.connectivity;
   late final _local = di.local;
+  final _pokemonNotifier = ValueNotifier<List<PokemonApiModel>>([]);
   late final _remote = di.remote;
+
+  ValueListenable<List<PokemonApiModel>> watchAll() => _pokemonNotifier;
+
+  ValueListenable<PokemonApiModel> watch(int id) {
+    final selectedPokemonNotifier =
+        ValueNotifier<PokemonApiModel>(PokemonApiModel());
+
+    void updateSelectedPokemon() {
+      selectedPokemonNotifier.value = _pokemonNotifier.value
+          .firstWhere((p) => p.id == id, orElse: () => PokemonApiModel());
+    }
+
+    updateSelectedPokemon();
+
+    _pokemonNotifier.addListener(updateSelectedPokemon);
+
+    return selectedPokemonNotifier;
+  }
 
   Future<void> deleteLast() async {
     await _local.deleteLast();
+    _updatePokemonNotifier();
   }
 
   Future<void> create(int id) async {
@@ -16,6 +38,7 @@ class PokemonRepository2 {
     }
     final pokemon = await _remote.read(id);
     await _local.create(pokemon);
+    _updatePokemonNotifier();
   }
 
   PokemonApiModel read(int id) {
@@ -24,9 +47,19 @@ class PokemonRepository2 {
 
   Future<void> update(PokemonApiModel pokemon) async {
     await _local.update(pokemon);
+    _updatePokemonNotifier();
   }
 
   List<PokemonApiModel> readAll() {
     return _local.readAll();
+  }
+
+  // void dispose() {
+  //   _pokemonNotifier.dispose();
+  //   _selectedPokemonNotifier.dispose();
+  // }
+
+  void _updatePokemonNotifier() {
+    _pokemonNotifier.value = readAll();
   }
 }
