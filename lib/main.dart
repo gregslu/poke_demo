6af9e9f4b6b34f702c418a8158cc3952be852cef
gregslu <viszu84@gmail.com>
2025-03-20 21:20:19 +0100

    Set preferred device orientation to portrait in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:poke_demo/src/di.dart';
 
 import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  );
   await di.initialize();
   runApp(const MyApp());
 }
